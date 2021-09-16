1885496f4a1ddd815583e129a4d06fcb54dd8596
Cliffy57 <73029436+Cliffy57@users.noreply.github.com>
2021-09-16 16:05:48 +0200

    Classes metier

---
+++ CPOA_TD1/src/net/cpoa/metier/Periodicite.java
@@ -0,0 +1,34 @@
+package net.cpoa.metier;
+
+public class Periodicite 
+{
+    private int ID;
+    private String libelle;
+
+    public Periodicite(int id, String libelle)
+    {
+        this.setID(id);
+        this.setLibelle(libelle);
+    }
+
+    public int getID() {
+        return ID;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    public String getLibelle() {
+        return libelle;
+    }
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    @Override
+    public String toString() {
+        return "Periodicite [ID=" + ID + ", libelle=" + libelle + "]";
+    }
+
+
+
+}
---
+++ CPOA_TD1/src/net/cpoa/metier/Revue.java
@@ -0,0 +1,62 @@
+package net.cpoa.metier;
+
+public class Revue {
+	
+	private int id;
+	private String titre;
+	private String description;
+	private float tarif_numero;
+	private String visuel;
+	private int id_periodicite;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public float getTarif_numero() {
+		return tarif_numero;
+	}
+	public void setTarif_numero(float tarif_numero) {
+		this.tarif_numero = tarif_numero;
+	}
+	public String getVisuel() {
+		return visuel;
+	}
+	public void setVisuel(String visuel) {
+		this.visuel = visuel;
+	}
+	public int getId_periodicite() {
+		return id_periodicite;
+	}
+	public void setId_periodicite(int id_periodicite) {
+		this.id_periodicite = id_periodicite;
+	}
+	public Revue(int id , String description, String titre , int tarif_numero,String visuel,int id_periodicite) {
+		this.id=id;
+		this.description=description;
+		this.titre=titre;
+		this.visuel=visuel;
+		this.tarif_numero=tarif_numero;
+		this.id_periodicite=id_periodicite;
+	}
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", titre=" + titre + ", description=" + description + ", tarif_numero=" + tarif_numero + ", visuel=" + visuel
+				+ ", id_periodicite=" + id_periodicite +"]";
+	}
+	
+	
+}
