014bb3d2940517f74f634e507725aeb17d73ce00
Cliffy57 <73029436+Cliffy57@users.noreply.github.com>
2021-09-21 19:36:28 +0200

    ClasseDAO

--- CPOA_TD1/src/net/cpoa/DAO/MySQLAbonnementDAO.java
+++ CPOA_TD1/src/net/cpoa/DAO/MySQLAbonnementDAO.java
@@ -1,5 +1,82 @@
 package net.cpoa.DAO;
 
-public class MySQLAbonnementDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+
+import net.cpoa.Connexion;
+import net.cpoa.DAO.metier.AbonnementMetier;
+
+public class MySQLAbonnementDAO implements DAO<AbonnementMetier> {
+
+	@Override
+	public AbonnementMetier getById(int id) {
+		AbonnementMetier periodicite =null;
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("select * from Abonnement where id_abonnement=?");
+			requete.setInt(1, id);
+			ResultSet res = requete.executeQuery();
+			if (res != null)
+				res.close();
+			if (requete != null)
+				requete.close();
+			if(laConnexion != null)
+				laConnexion.close();
+			} catch (SQLException sqle) {
+				System.out.println("Pb dans select " + sqle.getMessage());
+			}
+		
+		return periodicite;
+	}
+
+	@Override
+	public boolean create(AbonnementMetier objet) {
+		int nbLignes = 0;
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("INSERT INTO `dipaolo6u_cpoatdun`.`Abonnement` (`id_abonnement`, `voie`) VALUES (?, ?);");
+			requete.setInt(1, objet.getId());
+			requete.setInt(2, objet.getIDClient());
+			nbLignes = requete.executeUpdate();
+			
+			
+		} catch (SQLException sqle) {
+			System.out.println("Pb select" + sqle.getMessage());
+		}
+		return nbLignes ==1;
+	 }
+
+	@Override
+	public boolean update(AbonnementMetier objet) {
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("UPDATE `dipaolo6u_cpoatdun`.`Abonnement` SET `id_abonnement` = ?, `voie` = ? WHERE `Abonnement`.`id_abonnement` = ?;");
+			requete.setInt(1,objet.getId());
+			requete.setInt(2, objet.getIDClient());
+			requete.setInt(3, objet.getId());
+			int res = requete.executeUpdate();
+			
+		} catch (SQLException sqle) {
+			System.out.println("Pb select" + sqle.getMessage());
+		}		return false;
+	}
+
+	@Override
+	public boolean delete(AbonnementMetier objet) {
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("delete from Abonnement where id_abonnement=" + objet.getId());
+			int res = requete.executeUpdate();
+			
+		} catch (SQLException sqle) {
+			System.out.println("Pb select" + sqle.getMessage());
+		}
+	
+		return false;
+	}
+
+	
 }
--- CPOA_TD1/src/net/cpoa/DAO/MySQLClientDAO.java
+++ CPOA_TD1/src/net/cpoa/DAO/MySQLClientDAO.java
@@ -1,5 +1,82 @@
 package net.cpoa.DAO;
 
-public class MySQLClientDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+
+import net.cpoa.Connexion;
+import net.cpoa.DAO.metier.ClientMetier;
+
+public class MySQLClientDAO implements DAO<ClientMetier> {
+
+	@Override
+	public ClientMetier getById(int id) {
+		ClientMetier periodicite =null;
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("select * from Client where id_client=?");
+			requete.setInt(1, id);
+			ResultSet res = requete.executeQuery();
+			if (res != null)
+				res.close();
+			if (requete != null)
+				requete.close();
+			if(laConnexion != null)
+				laConnexion.close();
+			} catch (SQLException sqle) {
+				System.out.println("Pb dans select " + sqle.getMessage());
+			}
+		
+		return periodicite;
+	}
+
+	@Override
+	public boolean create(ClientMetier objet) {
+		int nbLignes = 0;
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("INSERT INTO `dipaolo6u_cpoatdun`.`Client` (`id_client`, `voie`) VALUES (?, ?);");
+			requete.setInt(1, objet.getId());
+			requete.setString(2, objet.getVoie());
+			nbLignes = requete.executeUpdate();
+			
+			
+		} catch (SQLException sqle) {
+			System.out.println("Pb select" + sqle.getMessage());
+		}
+		return nbLignes ==1;
+	 }
+
+	@Override
+	public boolean update(ClientMetier objet) {
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("UPDATE `dipaolo6u_cpoatdun`.`Client` SET `id_client` = ?, `voie` = ? WHERE `Client`.`id_client` = ?;");
+			requete.setInt(1,objet.getId());
+			requete.setString(2, objet.getVoie());
+			requete.setInt(3, objet.getId());
+			int res = requete.executeUpdate();
+			
+		} catch (SQLException sqle) {
+			System.out.println("Pb select" + sqle.getMessage());
+		}		return false;
+	}
+
+	@Override
+	public boolean delete(ClientMetier objet) {
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("delete from Client where id_client=" + objet.getId());
+			int res = requete.executeUpdate();
+			
+		} catch (SQLException sqle) {
+			System.out.println("Pb select" + sqle.getMessage());
+		}
+	
+		return false;
+	}
+
+	
 }
