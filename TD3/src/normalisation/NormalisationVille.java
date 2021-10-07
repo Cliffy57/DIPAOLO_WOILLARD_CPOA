f0ea1a6e87ecb92585e94421ce577c1c42988f88
woillard2u <woillard2u@iutm-inf-f13-09.iutmetz.site.univ-lorraine.fr>
2021-10-07 17:27:23 +0200

    voie test + normalisation(pas fini)

--- TD3/src/normalisation/NormalisationVille.java
+++ TD3/src/normalisation/NormalisationVille.java
@@ -4,47 +4,48 @@ public class NormalisationVille {
 
 	public static String normalisation(String str) {
 		
-		str = str.trim();
-		char firstLetter = str.charAt(0);
-		if(!(Character.isUpperCase(firstLetter)))
-		{
-			str = str.replaceFirst(Character.toString(firstLetter), Character.toString(Character.toUpperCase(firstLetter)));
+		if(str != null) {
+			str = str.trim();
+			char firstLetter = str.charAt(0);
+			if(!(Character.isUpperCase(firstLetter)))
+			{
+				str = str.replaceFirst(Character.toString(firstLetter), Character.toString(Character.toUpperCase(firstLetter)));
+			}
+			if(str.contains(" lès "))
+			{
+				str = str.replace(" lès ", "-lès-");
+			}
+			if(str.contains(" le "))
+			{
+				str = str.replace(" le ", "-le-");
+			}
+			if(str.contains(" sous "))
+			{
+				str = str.replace(" sous ", "-sous-");
+			}
+			if(str.contains(" sur "))
+			{
+				str = str.replace(" sur ", "-sur-");
+			}
+			if(str.contains(" à "))
+			{
+				str = str.replace(" à ", "-à-");
+			}
+			if(str.contains(" aux "))
+			{
+				str = str.replace(" aux ", "-aux-");
+			}
+			if(str.contains("St ") || str.contains("St-"))
+			{
+				str = str.replace("St ", "Saint-");
+				str = str.replace("St-", "Saint-");
+			}
+			if(str.contains("Ste ") || str.contains("Ste-"))
+			{
+				str = str.replace("Ste ", "Sainte-");
+				str = str.replace("Ste-", "Sainte-");
+			}
 		}
-		if(str.contains(" lès "))
-		{
-			str = str.replace(" lès ", "-lès-");
-		}
-		if(str.contains(" le "))
-		{
-			str = str.replace(" le ", "-le-");
-		}
-		if(str.contains(" sous "))
-		{
-			str = str.replace(" sous ", "-sous-");
-		}
-		if(str.contains(" sur "))
-		{
-			str = str.replace(" sur ", "-sur-");
-		}
-		if(str.contains(" à "))
-		{
-			str = str.replace(" à ", "-à-");
-		}
-		if(str.contains(" aux "))
-		{
-			str = str.replace(" aux ", "-aux-");
-		}
-		if(str.contains("St ") || str.contains("St-"))
-		{
-			str = str.replace("St ", "Saint-");
-			str = str.replace("St-", "Saint-");
-		}
-		if(str.contains("Ste ") || str.contains("Ste-"))
-		{
-			str = str.replace("Ste ", "Sainte-");
-			str = str.replace("Ste-", "Sainte-");
-		}
-		
 		return str;
 			
 	}
