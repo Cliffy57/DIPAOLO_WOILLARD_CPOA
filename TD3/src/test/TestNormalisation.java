709850f93b72e89c763dea6f9661250de9cf043f
woillard2u <woillard2u@iutm-inf-f13-09.iutmetz.site.univ-lorraine.fr>
2021-10-07 14:24:13 +0200

    debut des tests

--- TD3/src/test/TestNormalisation.java
+++ TD3/src/test/TestNormalisation.java
@@ -1,5 +1,14 @@
 package test;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 public class TestNormalisation {
 
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 }
